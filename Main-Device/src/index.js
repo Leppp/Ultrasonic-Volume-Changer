5411ed6d6a59ddb0dcc14e0840e226177b0b259b
Leppp <41381401+Leppp@users.noreply.github.com>
2018-07-26 14:05:02 -0400

    Updated index.js
    Added comments

--- Main-Device/src/index.js
+++ Main-Device/src/index.js
@@ -3,8 +3,8 @@ import { Observable, Subject, ReplaySubject, from, of, range } from 'rxjs';
 import { map, filter, switchMap } from 'rxjs/operators';
 //let json = require('../path/jsonfile.json');
 let rxUdp = require("rx-udp");
-let max = 15
-let min = 5
+let max = 15 // over this value the results aren't used to change the volume
+let min = 5 // under this value the results aren't used to change the volume
 let dgram = require('dgram');
 let server = dgram.createSocket('udp4');
 let exec = require('child_process').exec, child;
@@ -13,11 +13,12 @@ server.bind(5000);
 
 function change_volume(volume) {
 	let vol = Math.trunc(volume);
-	let cmd ='amixer -c 0 sset Master ' + vol  + '% unmute cap';
+	let cmd ='amixer -c 0 sset Master ' + vol  + '% unmute cap'; // sets the volume, change this line if you want to use it for other purposes
 	exec(cmd);
 	
 }
 observable.map((x) => JSON.parse(x)).filter((x) => x.distance < max && x.distance > min).map((x) => (x.distance - min) * (100/(max - min))).subscribe((x) => { change_volume(x) 
 console.log(x)
+// with the (x.distance - min) * (100/(max - min))) it changes the 5 to 15 range toa 0 to 100 range (change it for other purposes than changing the volume)
 });
 
